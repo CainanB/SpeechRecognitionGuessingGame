716ec71de340ac22a7447aa5107e82a48bb647bf
Cainan Barboza <CainanBarboza1@gmail.com>
2020-06-13 01:19:40 -0500

    Added list of previously guessed numbers

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # A simple Number guessing game
 ## Description
 Utilizes the experimental Speech Recognition Web API
+to process user guessed number and compare to randomly
+generated number.
 
 - Must allow browser to use microphone to play
 
@@ -9,3 +11,9 @@ Utilizes the experimental Speech Recognition Web API
 - Speech recognition processes user speech
 - Let user know if number is higher or lower than user guess
 - Will also reprompt user if guess is not within range or isn't a number
+
+### Possible improvements
+- Add sound effects
+- Show total amount of guesses
+- Limit Total allowed guesses 
+- Keep high score record
--- app.js
+++ app.js
@@ -4,17 +4,19 @@ var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEv
 
 var recognition = new SpeechRecognition();
 recognition.start();
-
+const numbersGuessedP = document.querySelector("#numbersGuessed");
 const randomNum = getRandomNumber();
 const message = document.querySelector("#horl");
 const userNumberGuessMessage = document.querySelector("#numberGuessed");
 const playAgain = document.querySelector("#playAgain");
+let guesses = [];
+
 
 function checkNumGuessed(num){
     // alert(num);
     const numChosen = parseInt(num);
     if(Number.isNaN(numChosen)){
-        userNumberGuessMessage.innerHTML= `You guessed ${num}`;
+        userNumberGuessMessage.innerText = `You guessed ${num}`;
         message.innerHTML = "That isn't a number Try Again!";
         return;
     }
@@ -28,15 +30,28 @@ function checkNumGuessed(num){
         message.innerHTML = `You chose correctly! The random number was ${randomNum}`;
         playAgain.innerHTML = `<button type="button" class="btn btn-light" id="play-again">Play Again</button>`
     }else if(numChosen > randomNum){
+        addGuessList(numChosen);
         userNumberGuessMessage.innerHTML= `You guessed ${numChosen}`;
         message.innerHTML = "The number is LOWER!";
+        // guesses.push(numChosen);
+        // numbersGuessedP.innerHTML = guesses.join();
+
     }else{
+        addGuessList(numChosen);
         userNumberGuessMessage.innerHTML= `You guessed ${numChosen}`;
         message.innerHTML = "The number is HIGHER!";
+        // guesses.push(numChosen);
+        // numbersGuessedP.innerHTML = guesses.join();
     }
 
 }
-
+function addGuessList(num){
+    //alert("called");
+    guesses.push(num);
+    //alert(guesses.toString());
+    const guessString = guesses.join();
+    numbersGuessedP.innerHTML = guessString;
+}
 function getRandomNumber(){
     const randomNum = Math.floor((Math.random() * 100) + 1);
     console.log(randomNum);
